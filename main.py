77490b2e1bf7eb1d8e63e3ae3696f04a90370b5c
DraugoLinn <kraszewski.mateusz@gmail.com>
2019-08-16 19:50:10 +0200

    Start

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import re
+import csv
+allowedCharacers = []
+vocabList = []
+kana = []
+allowedRegEx = ""
+kanaRegEx = ""
+with open('known-kanji.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        allowedCharacers.append(line[0])
+with open('kana.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for line in csv_reader:
+        kana.append(line[0])
+    for line in csv_reader:
+        allowedCharacers.append(line[0])
+
+with open('wordlist.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        vocabList.append(line[0])
+
+for list in allowedCharacers:
+    allowedRegEx = allowedRegEx + list
+for list in kana:
+    kanaRegEx = kanaRegEx + list
+
+for list in vocabList:
+    kanaOnly = bool(re.match("^[" + kanaRegEx + "]+$", list))
+    kanjiKnown = bool(re.match("^[" + allowedRegEx + "]+$", list))
+    if kanjiKnown == True:
+        if kanaOnly == True:
+            print(list, " kana only word")
+        else:
+            print(list, " word with all kanji known")
+    else:
+        print(list, " word with unknown kanji")
